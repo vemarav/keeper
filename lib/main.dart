dd90d2cb93c531ca37d13b9e32ed82cb788a52b6
aravind99 <vemula.aravind336@gmail.com>
2018-02-10 22:40:28 +0530

    Todos marked as complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-// TODO (3) create constant String for accountName as _AccountName
+// COMPLETE (3) create constant String for accountName as _AccountName
 const String _AccountName = 'Aravind Vemula';
-// TODO (4) create constant String for accountEmail as _AccountEmail
+// COMPLETE (4) create constant String for accountEmail as _AccountEmail
 const String _AccountEmail = 'vemula.aravind336@gmail.com';
-// TODO (5) create constant String for _AccountAbbr
+// COMPLETE (5) create constant String for _AccountAbbr
 const String _AccountAbbr = 'AV';
 
 void main() => runApp(new Keeper());
@@ -23,16 +23,16 @@ class Keeper extends StatelessWidget {
 
 class KeeperDrawer extends StatelessWidget {
 	
-	// TODO (1) create new Column as drawer child in Drawer constructor
+	// COMPLETE (1) create new Column as drawer child in Drawer constructor
 	// refer: https://docs.flutter.io/flutter/widgets/Column-class.html
-	// TODO (2) create UserAccountsDrawerHeader in Column children as first child
+	// COMPLETE (2) create UserAccountsDrawerHeader in Column children as first child
 	// refer: https://docs.flutter.io/flutter/material/UserAccountsDrawerHeader-class.html
-	// TODO (5) pass _AccountName and _AccountEmail to UserAccountsDrawerHeader as Text(String)
-	// TODO (6) create CircleAvatar as currentAccountPicture in UserAccountsDrawerHeader constructor
+	// COMPLETE (5) pass _AccountName and _AccountEmail to UserAccountsDrawerHeader as Text(String)
+	// COMPLETE (6) create CircleAvatar as currentAccountPicture in UserAccountsDrawerHeader constructor
 	// refer: https://docs.flutter.io/flutter/material/CircleAvatar-class.html
-	// TODO (7) pass _AccountAbbr as Text(String) to CircleAvatar child
+	// COMPLETE (7) pass _AccountAbbr as Text(String) to CircleAvatar child
 	// Now run app and you will see a Nice DrawerHeader
-	// TODO (8) As an Exercise try adding CircleAvatar backgroundImage and create Pull Request.
+	// COMPLETE (8) As an Exercise try adding CircleAvatar backgroundImage and create Pull Request.
 	
 	Drawer drawer = new Drawer(
 			child: new Column(children: <Widget>[
