18b8318ab70a074d8a9fe5078aa9db2b109f7ac4
aravind99 <vemula.aravind336@gmail.com>
2018-02-11 09:13:13 +0530

    Drawer complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-// TODO (3) create constant String for accountName as _AccountName
 const String _AccountName = 'Aravind Vemula';
-// TODO (4) create constant String for accountEmail as _AccountEmail
 const String _AccountEmail = 'vemula.aravind336@gmail.com';
-// TODO (5) create constant String for _AccountAbbr
 const String _AccountAbbr = 'AV';
 
 void main() => runApp(new Keeper());
@@ -14,6 +11,9 @@ class Keeper extends StatelessWidget {
 	Widget build(BuildContext context) {
 		return new MaterialApp(
 			title: 'Keeper',
+			theme: new ThemeData(
+				primaryColor: Colors.yellow.shade700
+			),
 			home: new KeeperDrawer(),
 		);
 	}
@@ -23,41 +23,161 @@ class Keeper extends StatelessWidget {
 
 class KeeperDrawer extends StatelessWidget {
 	
-	// TODO (1) create new Column as drawer child in Drawer constructor
-	// refer: https://docs.flutter.io/flutter/widgets/Column-class.html
-	// TODO (2) create UserAccountsDrawerHeader in Column children as first child
-	// refer: https://docs.flutter.io/flutter/material/UserAccountsDrawerHeader-class.html
-	// TODO (5) pass _AccountName and _AccountEmail to UserAccountsDrawerHeader as Text(String)
-	// TODO (6) create CircleAvatar as currentAccountPicture in UserAccountsDrawerHeader constructor
-	// refer: https://docs.flutter.io/flutter/material/CircleAvatar-class.html
-	// TODO (7) pass _AccountAbbr as Text(String) to CircleAvatar child
-	// Now run app and you will see a Nice DrawerHeader
-	// TODO (8) As an Exercise try adding CircleAvatar backgroundImage and create Pull Request.
-	
-	Drawer drawer = new Drawer(
-			child: new Column(children: <Widget>[
-				new UserAccountsDrawerHeader(
-					accountName: const Text(_AccountName),
-					accountEmail: const Text(_AccountEmail),
-					currentAccountPicture: new CircleAvatar(
-						backgroundColor: Colors.brown,
-						child: new Text(_AccountAbbr)
-					),
-				)
-			])
-	);
+	// TODO (1) replace drawer with new Drawer(child: ...) in Scaffold
+	// TODO (2) replace Column with ListView
+	// refer: https://docs.flutter.io/flutter/widgets/ListView-class.html
+	// TODO (3) make Column(children:...) as ListView(children:...)
+	// TODO (4) add padding top 0 to ListView to remove white space
+	// TODO (5) add otherAccountPictures with GestureDetector
+	// refer: https://docs.flutter.io/flutter/widgets/GestureDetector-class.html
+	// TODO (6) create a callback for GestureDetector(onTap:...) and showDialog with "Account Switching not implemented"
+	// TODO (7) add some ListTile to ListView(children: ...)
+	// refer: https://docs.flutter.io/flutter/material/ListTile-class.html
+	// TODO (8) create a callback method for each ListTile with ShowDialog with "Not Implemented"
+	// TODO (9) Exercise make children Dynamic
 	
 	@override
 	Widget build(BuildContext context) {
 		return new Scaffold(
-				drawer: drawer,
-				appBar: new AppBar(
-					title: new Text('Keeper'),
-				),
-				body: new Center(
-						child: new Text('Hello World!')
+			drawer: new Drawer(
+				child: new ListView(
+					padding: const EdgeInsets.only(top: 0.0),
+					children: <Widget>[
+						new UserAccountsDrawerHeader(
+							accountName: const Text(_AccountName),
+							accountEmail: const Text(_AccountEmail),
+							currentAccountPicture: new CircleAvatar(
+								backgroundColor: Colors.brown,
+								child: new Text(_AccountAbbr)
+							),
+							otherAccountsPictures: <Widget>[
+								new GestureDetector(
+									onTap: () => _onTapOtherAccounts(context),
+									child: new Semantics(
+										label: 'Switch Account',
+										child: new CircleAvatar(
+											backgroundColor: Colors.brown,
+											child: new Text('SA'),
+										),
+									),
+								)
+							]
+						),
+						new ListTile(
+							leading: new Icon(Icons.lightbulb_outline),
+							title: new Text('Notes'),
+							onTap: () => _onListTileTap(context),
+						),
+						new Divider(),
+						new ListTile(
+							leading: new Text('Label'),
+							trailing: new Text('Edit'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.label),
+							title: new Text('Expense'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.label),
+							title: new Text('Inspiration'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.label),
+							title: new Text('Personal'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.label),
+							title: new Text('Work'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.add),
+							title: new Text('Create new label'),
+							onTap: () => _onListTileTap(context),
+						),
+						new Divider(),
+						new ListTile(
+							leading: new Icon(Icons.archive),
+							title: new Text('Archive'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.delete),
+							title: new Text('Trash'),
+							onTap: () => _onListTileTap(context),
+						),
+						new Divider(),
+						new ListTile(
+							leading: new Icon(Icons.settings),
+							title: new Text('Settings'),
+							onTap: () => _onListTileTap(context),
+						),
+						new ListTile(
+							leading: new Icon(Icons.help),
+							title: new Text('Help & feedback'),
+							onTap: () => _onListTileTap(context),
+						)
+					]
 				)
+			),
+			
+			appBar: new AppBar(
+				title: new Text('Keeper'),
+			),
+			body: new Center(
+				child: new Text('Hello World!')
+			)
 		);
+	}
+	
+	_onTapOtherAccounts(BuildContext context) {
+		Navigator.of(context).pop();
+		showDialog<Null>(
+			context: context,
+			child: new AlertDialog(
+				title: const Text('Account switching not implemented.'),
+				actions: <Widget>[
+					new FlatButton(
+						child: const Text('OK'),
+						onPressed: () {
+							Navigator.of(context).pop();
+						},
+					),
+				],
+			),
+		);
+	}
+	
+	_onListTileTap(BuildContext context) {
+		Navigator.of(context).pop();
+		showDialog<Null>(
+			context: context,
+			child: new AlertDialog(
+				title: const Text('Not Implemented'),
+				actions: <Widget>[
+					new FlatButton(
+						child: const Text('OK'),
+						onPressed: () {
+							Navigator.of(context).pop();
+						},
+					),
+				],
+			),
+		);
+	}
+	
+	_labelWidgets() {
+		return [
+			new ListTile(
+				leading: new Icon(Icons.label),
+				title: new Text('Work'),
+				onTap: () => _onListTileTap(context),
+			),
+		];
 	}
 	
 }
