849dfe42751b01157c82c53a109c61420d01523e
aravind99 <vemula.aravind336@gmail.com>
2018-02-13 14:30:08 +0530

    todos ccompleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:keeper/views/app.dart';
 
+// COMPLETE (1) create a separate file for each class
+// COMPLETE (2) create a StatelessWidget for Keeper drawer
+// COMPLETE (3) call new KeeperDrawer() instead of new Drawer()
+// COMPLETE (4) change Notes class to LabelView
+// Hint: create views folder and add files which represents the class entity
+
 void main() {
 	runApp(new Keeper());
 }
