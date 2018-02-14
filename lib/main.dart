2adc319bda7cf54bd31d4bbad80a1d8b5c46909f
aravind99 <vemula.aravind336@gmail.com>
2018-02-14 23:44:15 +0530

    todos added for label form

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:keeper/views/app.dart';
 
-// COMPLETE (1) create a separate file for each class
-// COMPLETE (2) create a StatelessWidget for Keeper drawer
-// COMPLETE (3) call new KeeperDrawer() instead of new Drawer()
-// COMPLETE (4) change Notes class to LabelView
-// Hint: create views folder and add files which represents the class entity
+// TODO (1) replace KeeperApp with LabelView
+// TODO (2) make LabelView StatefulWidget
+// TODO (3) add static routeName to '/' in LabelView
+// TODO (4) create LabelForm as StatefulWidget
+// TODO (5) add static routeName to '/labelForm'
+// TODO (6) create mock_data.dart and add global variables for labels
+// TODO (7) import mock_data in LabelView and pass labels to KeeperDrawer constructor
+// TODO (8) delete keeper_app.dart
+// if labels value changes then LabelViewState update's the view
+// using form we change the labels value so LabelViewState Widget
+// depends on labels hence it updates the drawer
+// * [Note] Usage of global variables is not recommended unless you
+// are using them for development purpose
 
 void main() {
 	runApp(new Keeper());
